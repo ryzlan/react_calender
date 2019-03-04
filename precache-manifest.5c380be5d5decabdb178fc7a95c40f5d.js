18d51f3e2bf5acbfef00c1b250a3d8202f489b18
Ruzlan Karim <ruzlankarim2@gmail.com>
2019-03-05 02:29:20 +0600

    Updates

---
+++ precache-manifest.5c380be5d5decabdb178fc7a95c40f5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8cc0d7cc7cf38fd611ca",
+    "url": "/react_calender/static/js/runtime~main.8cc0d7cc.js"
+  },
+  {
+    "revision": "0cc47270409ca0ed35ee",
+    "url": "/react_calender/static/js/main.0cc47270.chunk.js"
+  },
+  {
+    "revision": "c37cfe9b827e2e37a328",
+    "url": "/react_calender/static/js/2.c37cfe9b.chunk.js"
+  },
+  {
+    "revision": "0cc47270409ca0ed35ee",
+    "url": "/react_calender/static/css/main.efb3633d.chunk.css"
+  },
+  {
+    "revision": "0a5f0bc92fe718dc85b0fe42f8d9a3bd",
+    "url": "/react_calender/index.html"
+  }
+];
